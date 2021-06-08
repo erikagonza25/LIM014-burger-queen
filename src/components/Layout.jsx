ac5d277a2c1b944ff05334309077164d32f99604
erikagonza25 <erikagonza2013@gmail.com>
2021-06-08 13:03:54 -0500

    Header como componente

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from "./Header"
+
+const Layout = ({children}) => {
+  return ( <div className="Main">
+    <Header/>
+    {children}
+  </div> );
+}
+ 
+export default Layout;
