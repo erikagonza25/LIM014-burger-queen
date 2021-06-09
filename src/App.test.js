38e130fc80e3fe5e9fc93e854d2545553601983b
erikagonza25 <erikagonza2013@gmail.com>
2021-06-09 13:00:42 -0500

    Poniendo app.js en una carpeta para mejor modularizacion

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//import { render, screen } from '@testing-library/react';
+//import App from './App';
+const text = "Hola Mundo";
+test("Debe contener un texto", () => {
+  expect(text).toMach(/Mundo/);
 });
