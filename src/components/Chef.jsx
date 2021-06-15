09e16c2d834bf7a0d3575d1f5cb95935ef909c11
Erika <erikagonza2013@gmail.com>
2021-06-15 09:22:26 -0500

    Hacer cambios finales

--- src/components/Chef.jsx
+++ src/components/Chef.jsx
@@ -24,6 +24,7 @@ const Chef = () => {
       {orders.map((order) => (
         <ChefOrder key={order.id} order={order} />
       ))}
+      <p><a href="/" className="buttonHome">Exit</a></p>
     </section>
   );
 };
--- src/components/ChefOrder.test.js
+++ src/components/ChefOrder.test.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import {render,fireEvent} from '@testing-library/react';
+import ChefOrder from './ChefOrder';
+
+test('order chef',()=>{
+  const {container} = render(<ChefOrder /> )
+  const button= container.firstChild
+  fireEvent.click(button)
+  expect(button.textContent).toBe('')
+})
+
 
